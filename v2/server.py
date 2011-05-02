d24327be65ae1f450824faa3bce2de3d22320b23
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-05-02 13:52:52 -0300

    Server is spawning process to serves to workers and taks

--- v2/server.py
+++ v2/server.py
@@ -3,7 +3,7 @@ import os
 import time
 import zmq
 
-from multiprocessing import Process
+from multiprocessing import Process, Array
 from scipy.misc import imread
 from optparse import OptionParser
 
@@ -25,8 +25,14 @@ def serve_to_tasks_generators(address, shared_address):
         print msg
         if msg.startswith('NEW'):
             if accept:
-                new_tasks.send('OK')
-                accept = 0
+                try:
+                    task_address = msg.split()[1]
+                except IndexError:
+                    new_tasks.send('REPEAT')
+                else:
+                    new_tasks.send('OK')
+                    accept = 0
+                    shared_address.value = task_address
             else:
                 new_tasks.send('WAIT')
         elif msg.startswith('END'):
@@ -36,13 +42,37 @@ def serve_to_tasks_generators(address, shared_address):
             new_tasks.send('REPEAT')
 
 
+def serve_to_workers(address, shared_address):
+    context = zmq.Context()
+    workers_socket = context.socket(zmq.REP)
+    workers_socket.bind(address)
+
+    while 1:
+        msg = workers_socket.recv()
+        print msg
+        if msg.startswith('GET'):
+            if shared_address.value:
+                workers_socket.send('ADDRESS %s' % shared_address)
+            else:
+                workers_socket.send('REPEAT')
+        else:
+            workers_socket.send('REPEAT')
+
+
 def main():
     opcoes, args = parse_cli()
     tasks_address = opcoes.tasks
     works_address = opcoes.works
 
-    pg = Process(target=serve_to_tasks_generators, args=(tasks_address,))
+    shared_address = Array('c', 256)
+
+    pg = Process(target=serve_to_tasks_generators, args=(tasks_address,
+                                                         shared_address))
     pg.start()
+
+    pw = Process(target=serve_to_workers, args=(works_address, shared_address))
+    pw.start()
+
 
 if __name__ == '__main__':
     main()
