90d41069a62b7ba6aa11e9d0364b5879d0945e2f
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-05-02 13:42:00 -0300

    New server

--- v2/server.py
+++ v2/server.py
@@ -3,35 +3,46 @@ import os
 import time
 import zmq
 
+from multiprocessing import Process
 from scipy.misc import imread
 from optparse import OptionParser
 
 def parse_cli():
     parser = OptionParser()
-    parser.add_option('-i', '--input', dest='input_dir', default='fatias')
-    parser.add_option('-o', '--output', dest='output_dir', default='fatias')
-    parser.add_option('-a', '--address', dest='address', default='tcp://*:1324')
+    parser.add_option('-n', '--tasks', dest='tasks', default='tcp://*:1234')
+    parser.add_option('-w', '--works', dest='works', default='tcp://*:1233')
     opcoes, args = parser.parse_args()
     return opcoes, args
 
 
-def serve(input_dir, output_dir, address):
+def serve_to_tasks_generators(address, shared_address):
     context = zmq.Context()
-    image_send = context.socket(zmq.PUSH)
-    image_send.bind(address)
-
-    for arq in glob.glob(os.path.join(input_dir, '*')):
-        img = imread(arq)
-        image_send.send_pyobj(img)
+    new_tasks = context.socket(zmq.REP)
+    new_tasks.bind(address)
+    accept = 1
+    while 1:
+        msg = new_tasks.recv()
+        print msg
+        if msg.startswith('NEW'):
+            if accept:
+                new_tasks.send('OK')
+                accept = 0
+            else:
+                new_tasks.send('WAIT')
+        elif msg.startswith('END'):
+            new_tasks.send('CLOSE')
+            accept = 1
+        else:
+            new_tasks.send('REPEAT')
 
 
 def main():
     opcoes, args = parse_cli()
-    input_dir = opcoes.input_dir
-    output_dir = opcoes.output_dir
-    address = opcoes.address
+    tasks_address = opcoes.tasks
+    works_address = opcoes.works
 
-    serve(input_dir, output_dir, address)
+    pg = Process(target=serve_to_tasks_generators, args=(tasks_address,))
+    pg.start()
 
 if __name__ == '__main__':
     main()
